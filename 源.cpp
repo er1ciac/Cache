fd047003c3c253b803adcd451e5079da31345d84
Chinazhuzi <139056883+Chinazhuzi@users.noreply.github.com>
2024-04-19 17:02:23 +0800

    2
    1

--- 源.cpp
+++ 源.cpp
@@ -1,17 +1,42 @@
 #include<iostream>
 using namespace std;
 //direct mapped
-class Direct_Cache {
+class Cache {
 public:
 	bool valid;
 	int tag;
 	int data;
-	Direct_Cache() {
+	Cache() {
 		valid = false;
 		tag = -1;
 		data = -1;
 	}
 };
+//暂时不用继承的方法写代码了
+class size_Cache{
+public:
+	bool valid;
+	int tag;
+	int data;
+	size_Cache* pointer;
+	size_Cache(){
+		valid = false;
+		tag = -1;
+		data = -1;
+	}
+	size_Cache(int size) {
+		valid = false;
+		tag = -1;
+		data = -1;
+		for(int i=0;i<size-1;i++){
+			size_Cache * tool=pointer;
+			pointer=new size_Cache();
+			tool=tool.pointer;
+		}
+		//内部set用链表，因为要遍历比较组内每一个元素
+	}
+};
+//具体地址格式等着跑出来数据再说
 int search(Direct_Cache *cache, int address) {
 	int tag=address / 16;
 	for (int i = 0; i < 4; i++) {
@@ -22,7 +47,12 @@ int search(Direct_Cache *cache, int address) {
 	return -1;
 }
 //
-
+void Direst_Cache(Cache * Pointer,int size){
+	Cache * Pointer=new Cache[size];
+	for(int i=0;i<size;i++){
+		Pointer[i]=new Cache();
+	}
+}
 
 
 //set 
